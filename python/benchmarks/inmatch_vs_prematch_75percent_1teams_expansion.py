5ae20d4161b1b2d137b3e727e5a8db5f9d0fb947
jonay2000 <jonabent@gmail.com>
2021-06-01 18:29:14 +0200

    fix exp

--- python/benchmarks/inmatch_vs_prematch_75percent_1teams_expansion.py
+++ python/benchmarks/inmatch_vs_prematch_75percent_1teams_expansion.py
@@ -7,12 +7,11 @@ from python.benchmarks.inmatch_vs_prematch_75percent_1teams import generate_maps
 import pathlib
 
 from python.benchmarks.parse_map import MapParser
-from python.benchmarks.run_with_timeout import run_with_timeout
+from python.benchmarks.run_with_timeout_expansion import run_with_expansion_timeout
 from python.mstar.rewrite import Config, MatchingStrategy
 from python.mstar.rewrite.config import GigaByte
 from python.solvers.configurable_mstar_solver import ConfigurableMStar
 
-import copy
 
 this_dir = pathlib.Path(__file__).parent.absolute()
 name = "inmatch_vs_prematch_75percent_1teams_maps"
@@ -49,66 +48,47 @@ def run_benchmark():
     parser = MapParser(batchdir)
 
     # num agents : solutions
-    inmatch: dict[int, list[Optional[float]]] = {}
-    prematch: dict[int, list[Optional[float]]] = {}
-
-    inmatch_exp: dict[int, list[int]] = {}
-    prematch_exp: dict[int, list[int]] = {}
+    inmatch: dict[int, list[Optional[list[int]]]] = {}
+    prematch: dict[int, list[Optional[list[int]]]] = {}
 
     all_problems = [[i[1] for i in parser.parse_batch(name.name)] for name in batchdir.iterdir() if name.is_dir()]
     all_problems.sort(key=lambda i: len(i[0].goals))
+    with open(batchdir / "results_inmatch_expansion.txt", "w") as imf:
+        with open(batchdir / "results_inmatch_expansion.txt", "w") as pmf:
 
-    with Pool(processes) as p:
-        for problems in tqdm(all_problems):
-            num_agents = len(problems[0].goals)
+            with Pool(processes) as p:
+                for problems in tqdm(all_problems):
+                    num_agents = len(problems[0].goals)
 
-            if num_agents <= 1 or sum(1 for i in inmatch[num_agents - 1] if i is not None) != 0:
-                path = run_with_timeout(p, ConfigurableMStar(
-                    inmatch_config
-                ), problems, 2 * 60)
+                    if num_agents <= 1 or sum(1 for i in inmatch[num_agents - 1] if i is not None) != 0:
+                        expansions = run_with_expansion_timeout(p, ConfigurableMStar(
+                            inmatch_config
+                        ), problems, 2 * 60)
 
-                print(path)
-                tqdm.write("\n")
-                tqdm.write(f"{len(inmatch_config.expansions), sum(inmatch_config.expansions)}")
-                tqdm.write("\n")
+                        tqdm.write("\n\n\n")
+                        tqdm.write(f"{sum(len(i) for i in expansions), sum(sum(i) for i in expansions)}")
+                        tqdm.write("\n\n\n")
 
-                inmatch_exp[num_agents] = copy.deepcopy(inmatch_config.expansions)
-                inmatch_config.expansions = []
+                        inmatch[num_agents] = expansions
+                        imf.writelines([f"{num_agents}: {expansions}"])
+                    else:
+                        inmatch[num_agents] = [None for i in range(len(problems))]
 
-                inmatch[num_agents] = path
-            else:
-                inmatch[num_agents] = [None for i in range(len(problems))]
+                    if num_agents <= 1 or sum(1 for i in prematch[num_agents - 1] if i is not None) != 0:
+                        expansions = run_with_expansion_timeout(p, ConfigurableMStar(
+                            prematch_config
+                        ), problems, 2 * 60)
 
-            if num_agents <= 1 or sum(1 for i in prematch[num_agents - 1] if i is not None) != 0:
-                path = run_with_timeout(p, ConfigurableMStar(
-                    prematch_config
-                ), problems, 2 * 60)
+                        tqdm.write("\n\n\n")
+                        tqdm.write(f"{sum(len(i) for i in expansions), sum(sum(i) for i in expansions)}")
+                        tqdm.write("\n\n\n")
 
-                print(path)
-                tqdm.write("\n")
-                tqdm.write(f"{len(prematch_config.expansions), sum(prematch_config.expansions)}")
-                tqdm.write("\n")
-
-                prematch_exp[num_agents] = copy.deepcopy(prematch_config.expansions)
-                prematch_config.expansions = []
-
-
-                prematch[num_agents] = path
-            else:
-                prematch[num_agents] = [None for i in range(len(problems))]
+                        pmf.writelines([f"{num_agents}: {expansions}"])
+                        prematch[num_agents] = expansions
+                    else:
+                        prematch[num_agents] = [None for i in range(len(problems))]
 
 
-    tqdm.write(str(inmatch_exp))
-    tqdm.write(str(prematch_exp))
-
-    output_data(batchdir / "results_inmatch_expansion.txt", inmatch)
-    output_data(batchdir / "results_prematch_expansion.txt", inmatch)
-
-
-def output_data(file: pathlib.Path, data: dict[int, list[float]]):
-    with open(file, "w") as f:
-        for i, r in sorted([(a, b) for a, b in data.items()], key=lambda x: x[0]):
-            f.write(f"{i}: {r}")
 
 
 
