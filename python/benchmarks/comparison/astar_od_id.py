c98ba69b866e9b52834aa770de71a53bcc0ea981
Robbin Baauw <robbin.baauw@gmail.com>
2021-06-10 12:40:06 +0200

    Add A*-OD-ID benchmark setup

---
+++ python/benchmarks/comparison/astar_od_id.py
@@ -0,0 +1,33 @@
+from mapfmclient import Problem, Solution
+
+import sys
+import pathlib
+
+from python.benchmarks.comparison.util import get_src_modules, solve_with_modules
+
+this_dir = pathlib.Path(__file__).parent.absolute()
+
+from python.algorithm import MapfAlgorithm
+
+sys.path.insert(0, str(this_dir / "Astar-OD-ID"))
+print(sys.path)
+from src.util.grid import HeuristicType, Grid
+from src.main import solve
+sys.path.pop(0)
+
+modules = get_src_modules()
+
+# Configure algorithm
+heuristic_type = HeuristicType.Exhaustive
+enable_cat = True
+enable_id = True
+enable_sorting = False
+
+
+class AStarODID(MapfAlgorithm):
+    def solve(self, problem: Problem) -> Solution:
+        return solve_with_modules(modules, solve)
+
+    @property
+    def name(self) -> str:
+        return "A*-OD-ID (Ivar)"
