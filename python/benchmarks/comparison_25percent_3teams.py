df0b49feda7a07c212fbb417c769ac1a2683d7c4
jonay2000 <jonabent@gmail.com>
2021-06-12 09:05:12 +0200

    75perc 1 teams done

--- python/benchmarks/comparison_25percent_3teams.py
+++ python/benchmarks/comparison_25percent_3teams.py
@@ -5,6 +5,7 @@ from tqdm import tqdm
 
 from python.algorithm import MapfAlgorithm
 from python.benchmarks.comparison import EPEAStar, CBS, AStarODID
+from python.benchmarks.comparison.icts import ICTS
 from python.benchmarks.extensions_25percent_3teams import read_from_file
 from python.benchmarks.graph_times import graph_results
 from python.benchmarks.inmatch_vs_prematch_75percent_1teams import output_data
@@ -155,6 +156,18 @@ if __name__ == '__main__':
     files.append(run(
         lambda: AStarODID(),
         "A*-OD-ID"
+    ))
+
+    graph_results(
+        *files,
+        batchdir / f"{name}.png",
+        save=False,
+        bounds=False,
+    )
+
+    files.append(run(
+        lambda: ICTS(),
+        "ICTS"
     ))
 
     graph_results(
