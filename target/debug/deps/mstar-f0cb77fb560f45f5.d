27709aa22d00b3a6ede84023197fbd6289ad927e
jonay2000 <jonabent@gmail.com>
2021-06-13 18:14:11 +0200

    workspace

---
+++ target/debug/deps/mstar-f0cb77fb560f45f5.d
@@ -0,0 +1,5 @@
+/home/jonathan/src/research-project/target/debug/deps/mstar-f0cb77fb560f45f5: rust/src/main.rs
+
+/home/jonathan/src/research-project/target/debug/deps/mstar-f0cb77fb560f45f5.d: rust/src/main.rs
+
+rust/src/main.rs:
---
+++ target/debug/mstar.d
@@ -0,0 +1,1 @@
+/home/jonathan/src/research-project/target/debug/mstar: /home/jonathan/src/research-project/rust/src/main.rs
