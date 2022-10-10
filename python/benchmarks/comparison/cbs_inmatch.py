6443da84c59b585ff6cede7095942b96445fbbdf
Jeroen van Dijk <jeroen11dijk@gmail.com>
2022-10-10 12:57:13 +0200

    add try except

--- python/benchmarks/comparison/cbs_inmatch.py
+++ python/benchmarks/comparison/cbs_inmatch.py
@@ -57,21 +57,23 @@ class CBSSolver(MapfAlgorithm):
             print(e)
 
         paths = []
-
-        with open("paths.txt", "r") as f:
-            sol_val = int(f.readline())
-            re_p = re.compile("(\(\d+,\d+\))")
-            while True:
-                line = f.readline()
-                print(line)
-                if not line: break
-                path = []
-                for node in re_p.findall(line):
-                    node = node.replace("(", "").replace(")", "")
-                    print(node)
-                    x, y = node.split(",")
-                    path.append((int(x), int(y)))
-                paths.append(path)
+        try:
+            with open("paths.txt", "r") as f:
+                sol_val = int(f.readline())
+                re_p = re.compile("(\(\d+,\d+\))")
+                while True:
+                    line = f.readline()
+                    print(line)
+                    if not line: break
+                    path = []
+                    for node in re_p.findall(line):
+                        node = node.replace("(", "").replace(")", "")
+                        print(node)
+                        x, y = node.split(",")
+                        path.append((int(x), int(y)))
+                    paths.append(path)
+        except Exception as e:
+            print(e)
         return Solution.from_paths(paths)
 
     @property
