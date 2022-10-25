a3a1146c6e46db8755666544ad977c683c8e05da
Jeroen van Dijk <jeroen11dijk@gmail.com>
2022-10-25 12:47:04 +0200

    debugging method 4.0

--- python/benchmarks/comparison_25percent_3teams.py
+++ python/benchmarks/comparison_25percent_3teams.py
@@ -70,6 +70,12 @@ def run(solver: Callable[[], MapfAlgorithm], bm_name: str, parse_maps: bool = Tr
     results_costs: dict[int, list[Optional[float]]] = {}
 
     all_problems = [parser.parse_batch(n.name) for n in batchdir.iterdir() if n.is_dir()]
+    lst = list(range(1,51))
+    for problems in all_problems:
+        print(len(problems))
+        if len(problems) > 0:
+            lst.remove(len(problems[0][1].goals))
+    print(lst, len(all_problems))
     all_problems.sort(key=lambda i: len(i[0][1].goals))
     for problem_list in all_problems:
         for problem in problem_list:
