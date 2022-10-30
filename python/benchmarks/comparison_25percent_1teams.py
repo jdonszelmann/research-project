00613ff5379c181c524762d001d8bf93848fb47c
Jeroen van Dijk <jeroen11dijk@gmail.com>
2022-10-30 12:31:41 +0100

    fixed a small bug

--- python/benchmarks/comparison_25percent_1teams.py
+++ python/benchmarks/comparison_25percent_1teams.py
@@ -39,8 +39,8 @@ def generate_maps():
 
         map_generator = MapGenerator(path)
         map_generator.generate_even_batch(
-            20,  # number of maps
-            40, 40,  # size
+            10,  # number of maps
+            128, 128,  # size
             i,  # number of agents
             1,  # number of teams
             prefix=name,
@@ -81,7 +81,7 @@ def run(solver: Callable[[], MapfAlgorithm], bm_name: str, parse_maps: bool = Tr
             continue
         if num_agents <= 2 or sum(1 for i in results[num_agents - 1] if i is not None) != 0:
             # sols_inmatch = run_with_timeout(p, solver(), problems, parse_maps, 1 * 1) # test with low timeout
-            all_results = run_with_timeout(solver(), problems, parse_maps, 180)  # test with low timeout
+            all_results = run_with_timeout(solver(), problems, parse_maps, 60)  # test with low timeout
             sols_inmatch, _ = zip(*all_results)
             tqdm.write(f"{bm_name} with {num_agents} agents: {sols_inmatch}")
             results[num_agents] = sols_inmatch
