0b375c1e9f859d82fb718c7ac890e5b84d1b7524
Jeroen van Dijk <jeroen11dijk@gmail.com>
2022-11-07 15:00:58 +0100

    dots for the win

--- python/benchmarks/comparison/__init__.py
+++ python/benchmarks/comparison/__init__.py
@@ -5,7 +5,7 @@ from .bcpmapf_inmatch import BCPSolver as BCPInmatch
 from .bcpmapf_prematch import BCPSolver as BCPPrematch
 from .cbs_inmatch import CBSSolver as CBSInmatch
 from .cbs_prematch import CBSSolver as CBSPrematch
-from SATSolverPrematch import SATSolver as SATPrematch
-from SATSolverInmatch import SATSolverColored as SATInmatch
+from .SATSolverPrematch import SATSolver as SATPrematch
+from .SATSolverInmatch import SATSolverColored as SATInmatch
 
 __all__ = ["BCPInmatch", "BCPPrematch", "CBSInmatch", "CBSPrematch", "CBM", "SATPrematch", "SATInmatch"] #["EPEAStar", "CBM", "AStarODID", "ICTS"]
