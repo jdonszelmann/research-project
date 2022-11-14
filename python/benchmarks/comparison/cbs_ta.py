ac2c6bf9d09dc73ce6e40c28528b15867b60c7d3
Jeroen van Dijk <jeroen11dijk@gmail.com>
2022-11-14 14:54:48 +0100

    Maybe this works

--- python/benchmarks/comparison/cbs_ta.py
+++ python/benchmarks/comparison/cbs_ta.py
@@ -31,19 +31,19 @@ class CBSSolver(MapfAlgorithm):
         scenario_path = map_path.replace(".map", ".yaml")
         f = open(scenario_path, 'w')
         f.write("map:\n")
-        f.write("    dimensions: {}\n".format([problem.width, problem.height]))
-        f.write("    obstacles:\n")
+        f.write("  dimensions: {}\n".format([problem.width, problem.height]))
+        f.write("  obstacles:\n")
         for o in obstacles:
             f.write("      - {}\n".format(list(o)))
         f.write("agents:\n")
         for i in range(num_of_agents):
             start = [problem.starts[i].x, problem.starts[i].y]
             goal = goals[problem.starts[i].color]
-            f.write("""    - name: agent{}
-      start: {}\n""".format(i, start))
-            f.write("      potentialGoals:\n")
+            f.write("""  - name: agent{}
+    start: {}\n""".format(i, start))
+            f.write("    potentialGoals:\n")
             for g in goal:
-                f.write("        - {}".format(list(g)))
+                f.write("      - {}".format(list(g)))
                 f.write("\n")
         f.close()
 
